6fe77e74e1a6d9107c5bedbcf6a66334f1107b9f
alex <you@example.com>
2025-02-23 21:51:01 +0530

    bargraph done

--- script.js
+++ script.js
@@ -6,14 +6,16 @@
 // location
 // arrests
 
+const tickColor = "rgb(0,0,150)";
+
 const totalCases = 500;
 const totalRequests = 500;
 
 const casesData = [100, 80, 90, 120, 110];
 const requestsData = [100, 80, 90, 120, 110];
 
-const casesColors = ["blue", "orange", "yellow", "gray", "red"];
-const requestsColors = ["blue", "orange", "yellow", "gray", "red"];
+const casesColors = ["green", "yellow", "gray", "black", "darkblue"];
+const requestsColors = ["green", "yellow", "orange", "rgb(100,150,100)", "red"];
 
 const requestDataPercents = requestsData.map((requestsData) =>
   Math.floor((requestsData / totalRequests) * 100)
@@ -31,9 +33,9 @@ const casesLabels = [
 const requestsLabels = [
   "Successful",
   "Pending Case",
-  "In Progress",
-  "Closed Cases",
-  "Allowed But Not Started",
+  "Pending for approval",
+  "Approved but request not sent",
+  "Rejected Requests",
 ].map((caseLabel, i) => `${requestDataPercents[i]} - ${caseLabel}`);
 
 const chartoptions = {
@@ -99,16 +101,70 @@ const chartoptions = {
   //   },
 };
 
+const bargraphOptions = {
+  responsive: true,
+  maintainAspectRatio: false,
+
+  scales: {
+    x: {
+      border: {
+        width: 2,
+      },
+      grid: {
+        display: false,
+      },
+
+      ticks: {
+        // backdropColor: "#1f305e",
+        // backdropColor: "rgba(0,0,0,1)",
+        color: tickColor,
+      },
+    },
+
+    y: {
+      suggestedMin: 0,
+      suggestedMax: 120,
+
+      border: {
+        width: 2,
+      },
+      grid: {
+        display: false,
+      },
+      ticks: {
+        // backdropColor: "#1f305e",
+        // backdropColor: "rgba(0,0,0,1)",
+        color: tickColor,
+      },
+    },
+  },
+
+  maxBarThickness: 50,
+  barPercentage: 0.5,
+  borderRadius: {
+    topLeft: "5",
+    topRight: "5",
+  },
+  //   backgroundColor: "#1f305e",
+
+  plugins: {
+    legend: {
+      display: false,
+    },
+  },
+};
+
 document.querySelector("#casesNumbers").textContent = totalCases;
 document.querySelector("#requestsNumbers").textContent = totalRequests;
 
+// callback function to handle click event of hamburger icon
 function expand() {
   document.querySelector(".leftside").classList.toggle("expanded");
 }
 
 const totalRequestsCtx = document.querySelector("#totalRequestsChart");
-
 const totalCasesCtx = document.querySelector("#totalCasesChart");
+const casesBarChartCtx = document.querySelector("#casesBarChart");
 
 function totalCasesDraw() {
   const totalCasesChart = new Chart(totalCasesCtx, {
@@ -152,5 +208,23 @@ function totalRequestsDraw() {
   });
 }
 
+function casesBarChartDraw() {
+  const casesBarChart = new Chart(casesBarChartCtx, {
+    type: "bar",
+    data: {
+      labels: ["CDR", "IPDR", "PCAP", "ISD"],
+      datasets: [
+        {
+          data: [120, 90, 60, 100],
+          backgroundColor: "orange",
+        },
+      ],
+    },
+
+    options: bargraphOptions,
+  });
+}
+
 totalCasesDraw();
 totalRequestsDraw();
+casesBarChartDraw();
